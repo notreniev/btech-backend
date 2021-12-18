76b854f4bb8ee556bd45731aa9a352dcb6d54cf0
evertonsilva <evertonsilva@luizalabs.com>
2021-12-18 20:59:12 -0300

    wip: added routes for project CRUD and service operations

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProjectModel } from '../domains/models/project.model';
 import { ProjectService } from './project.service';
 
 @Controller('api/projects')
 export class ProjectController {
     constructor(private readonly projectService: ProjectService) {}
+
+    @Post()
+    async create(@Body() project: ProjectModel): Promise<ProjectModel>{
+        return await this.projectService.create(project);
+    }
+
+    @Patch('/:id')
+    async update(@Body() project: ProjectModel): Promise<ProjectModel>{
+        return await this.projectService.update(project);
+    }
 
     @Get()
     getProjects():Promise<ProjectModel[]>{
@@ -16,4 +26,8 @@ export class ProjectController {
         return await this.projectService.getProject(id);
     }
 
+    @Delete('/:id')
+    async remove(@Param('id') projectId: string){
+        return await this.projectService.delete(projectId);
+    }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -9,15 +9,29 @@ export class ProjectService {
 
     constructor(@InjectModel('Project') private readonly projectModel: Model<ProjectDocument>){}
 
+    create(project: ProjectModel): Promise<ProjectModel>{
+        const dbResult = this.projectModel.create(project);
+        return dbResult;
+    }
+
+    async update(project: ProjectModel): Promise<ProjectModel>{
+        const dbResult = await this.projectModel.findOneAndUpdate({_id: project._id}, project, { new: true});
+        return dbResult;
+    }
+
     async getProject(projectId: string): Promise<ProjectModel[]>{
         const dbResult = await this.projectModel.find({_id: projectId});
         return dbResult;
     }
 
     async getProjects(): Promise<ProjectModel[]>{
-        // const dbResult = await this.projectModel.find({});
-        // return dbResult;
-        return Promise.resolve(getProjectsMock());
+        const dbResult = await this.projectModel.find({});
+        return dbResult;
+    }
+
+    async delete(projectId: string): Promise<ProjectModel>{
+        const dbResult =  await this.projectModel.findOneAndDelete({_id: projectId});
+        return dbResult;
     }
 
 }
